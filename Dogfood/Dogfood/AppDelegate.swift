dc725c016f3dc2917c1046070ac4e5d38ebf35af
jinha <dkwlsfk2323@gmail.com>
2021-09-23 15:38:57 +0900

    프로젝트 초기 세팅

--- Dogfood/Dogfood/AppDelegate.swift
+++ Dogfood/Dogfood/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
---
+++ Dogfood/Pods/Target Support Files/Pods-Dogfood/Pods-Dogfood-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dogfood : NSObject
+@end
+@implementation PodsDummy_Pods_Dogfood
+@end
